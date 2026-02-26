afd30c4c1d94437732df24508bbe07cd193460d7
huni6711 <huni6711@naver.com>
2026-02-26 15:38:14 +0900

    first commit

---
+++ ex2/src/main/java/org/zerock/ex2/entity/Memo.java
@@ -0,0 +1,21 @@
+package org.zerock.ex2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity //엔티티 클래스
+@Table(name = "tbl_memo") //테이블명
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+}
